d11db53b878fd3d1c92341a66f6336282a4980ce
kangshuaibo <1025087682@qq.com>
2018-12-21 11:58:33 +0800

    77

--- chapter6/题8鞍点.c
+++ chapter6/题8鞍点.c
@@ -6,14 +6,15 @@
 int main(int argc, char const *argv[])
 {
 	int a[N][M];
-	//矩阵输入
+	
 
-	int i,j;
+	int i,j;//矩阵输入
 	for(i=0;i<N;i++)
 		for(j=0;j<M;j++)
 			scanf("%d",&a[i][j]);
-    //输出矩阵
-		printf("您的矩阵为：\n");
+    
+
+		printf("您的矩阵为：\n");//输出矩阵
 	for(i=0;i<N;i++)
 	{
 		for(j=0;j<M;j++)
@@ -22,11 +23,13 @@ int main(int argc, char const *argv[])
 		}
 	 printf("\n");
 	}
+
+
 	//找鞍点
 	int max,maxj,flag,k;
-	for(i=0;i<=N;i++)//从第一行开始
+	for(i=0;i<=N;i++)    //从第一行开始
 	{
-		max=a[i][0];//假设第一列这个是最大的
+		max=a[i][0];     //假设第一列这个是最大的
 	    maxj=0;
 	    for(j=0;j<M;j++)//遍历第一行中元素
 	    	if(a[i][j]>max)
@@ -35,7 +38,7 @@ int main(int argc, char const *argv[])
 	    	    maxj=j;
 	    	}
 
-	   flag=1;//假设是鞍点
+	   flag=1;          //假设是鞍点
 	 
 	    for(k=0;k<N;k++)//遍历第一列元素
 	      {
@@ -47,6 +50,7 @@ int main(int argc, char const *argv[])
 	    if(flag)
 	 	printf("存在鞍点a[%d][%d]=%d\n",i,maxj,max);break;
 	}
+	
     if(!flag)
     {
     	printf("不存在鞍点");
--- chapter7/题18给出年月日计算该日是该年第几天.c
+++ chapter7/题18给出年月日计算该日是该年第几天.c
@@ -3,27 +3,32 @@ int main(int argc, char const *argv[])
 {
 	int year,month,day,days;
 
-	int sum_day(int month,int day);
+	int sum_day(int month,int day);//声明
 	int leap(int year);
     
-    printf("请输入年月日：\n");
+    printf("请输入年月日：\n");		//输入
 	scanf("%d%d%d",&year,&month,&day);
 
-	days=sum_day(month,day);
+	days=sum_day(month,day);	//days为第几天 闰年需要加1（除1、2月）
 		if( leap(year)&&month>=3 )
 		{
 			days=days+1;
 		}
 	printf("是第%d天\n",days );
+
+
+
 	return 0;
 }
 
-int sum_day(int month,int day)
+int sum_day(int month,int day)	//计算每个月份的和 加上本月的天
 {
  int day_tab[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
  int i;
- for(i=1;i<=month;i++)//这里书中错误❌
- 	day=day+day_tab[i];
+ for(i=1;i<=month;i++)//这里书中错误❌。 
+ 	day=day+day_tab[i];	//此处为关键😄如6月18日为前六个月的天数相加 再加上18
+
  return day;
 }
 
@@ -31,7 +36,7 @@ int leap(int year)
 { int n=1;
   if(year%4!=0)
   	n=0;
-  	else if(year%100!=0)
+  	  else if(year%100!=0)
   		n=1;
   		  else if(year%400!=0)
   			n=0;
